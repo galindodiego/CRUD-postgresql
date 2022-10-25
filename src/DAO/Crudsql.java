e6faa8077bc63f685f6af3df5d2272895f2a2a12
Diego Galindo <diegogalindo84@gmail.com>
2022-10-25 18:03:16 -0300

    Update inserido

--- src/DAO/Crudsql.java
+++ src/DAO/Crudsql.java
@@ -70,4 +70,37 @@ public class Crudsql extends ConexaoSql {
 
         }
     }
+    
+    public void update(EmpregadoDTO objEmpregadoDto){
+        try {
+            Connection conexao = conectar();
+            String sql = "update empregados set nome=?,sobrenome=?,funcao=? where id_empregado=?";
+            pstm = conexao.prepareStatement(sql);
+            pstm.setString(1, objEmpregadoDto.getNome());
+            pstm.setString(2, objEmpregadoDto.getSobrenome());
+            pstm.setString(3, objEmpregadoDto.getFuncao());
+            pstm.setInt(4, objEmpregadoDto.getIdempregado());
+            pstm.execute();
+            pstm.close();
+            conexao.close();
+            JOptionPane.showMessageDialog(null,"Empregado atualizado");
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null,"Empregado não atualizado "+erro);
+        }
+    }
+    
+    public void delete(EmpregadoDTO objEmpregadoDto){
+        try {
+            Connection conexao = conectar();
+            String sql = "delete from empregados where id_empregado=?";
+            pstm = conexao.prepareStatement(sql);
+            pstm.setInt(1,objEmpregadoDto.getIdempregado());
+            pstm.execute();
+            pstm.close();
+            conexao.close();
+             JOptionPane.showMessageDialog(null,"Empregado deletado");
+        } catch (Exception erro) {
+             JOptionPane.showMessageDialog(null,"Empregado não deletado "+erro);
+        }
+    }
 }
--- src/VIEW/pesquisar.java
+++ src/VIEW/pesquisar.java
@@ -41,11 +41,16 @@ public class pesquisar extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         btnVoltar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("ID empregado :");
 
+        txtId.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        txtId.setEnabled(false);
         txtId.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtIdActionPerformed(evt);
@@ -74,11 +79,28 @@ public class pesquisar extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("ATUALIZAR EMPREGADO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("DELETAR EMPREGADO");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel6.setText("DADOS DO EMPREGADO");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
@@ -89,22 +111,28 @@ public class pesquisar extends javax.swing.JFrame {
                             .addComponent(jLabel5)
                             .addComponent(jLabel2)
                             .addComponent(jLabel3)
-                            .addComponent(jLabel4))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                            .addComponent(jLabel4)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnVoltar)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtFuncao, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(txtidEmpregado, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnPesquisar))
-                    .addComponent(txtSobrenome, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(btnVoltar)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(txtSobrenome, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtFuncao, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(33, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel6)
+                .addGap(65, 65, 65))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,7 +142,9 @@ public class pesquisar extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(btnPesquisar)
                     .addComponent(txtidEmpregado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(64, 64, 64)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel6)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
@@ -130,7 +160,11 @@ public class pesquisar extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtFuncao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
                 .addComponent(btnVoltar)
                 .addContainerGap())
         );
@@ -153,6 +187,15 @@ public class pesquisar extends javax.swing.JFrame {
         pesquisarFuncionario();
 
     }//GEN-LAST:event_btnPesquisarActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        atualizarEmpregado();
+        limpar();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -192,11 +235,14 @@ public class pesquisar extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnPesquisar;
     private javax.swing.JButton btnVoltar;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JTextField txtFuncao;
     private javax.swing.JTextField txtId;
     private javax.swing.JTextField txtNome;
@@ -214,5 +260,28 @@ public class pesquisar extends javax.swing.JFrame {
         txtSobrenome.setText(objEmpregadoDto.getSobrenome());
         txtFuncao.setText(objEmpregadoDto.getFuncao());
     }
+
+    private void atualizarEmpregado() {
+        EmpregadoDTO objEmpregadoDto = new EmpregadoDTO();
+        objEmpregadoDto.setIdempregado(Integer.parseInt(txtidEmpregado.getText()));
+        objEmpregadoDto.setNome(txtNome.getText());
+        objEmpregadoDto.setSobrenome(txtSobrenome.getText());
+        objEmpregadoDto.setFuncao(txtFuncao.getText());
+        Crudsql crudAtualizar = new Crudsql();
+        crudAtualizar.update(objEmpregadoDto);
+
+    }
+    private void delete(){
+        
+    }
+    
+    private void limpar(){
+        txtId.setText("");
+        txtNome.setText("");
+        txtSobrenome.setText("");
+        txtFuncao.setText("");
+    }
+    
+    
 
 }
