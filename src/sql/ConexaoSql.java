5a33d67a36184ca6cbbd75a03223990642eff22e
Diego Galindo <diegogalindo84@gmail.com>
2022-10-25 10:11:10 -0300

    first commit

---
+++ src/sql/ConexaoSql.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Diego
+ */
+public class ConexaoSql {
+
+    Connection conn = null;
+    String url = "jdbc:postgresql://localhost:5432/empresa";
+
+    String usuario = "postgres";
+    String password = "123";
+
+    public void conectar() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, usuario, password);
+            JOptionPane.showMessageDialog(null, "Conectado", "conexão", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Error ao Conectar", erro + "conexão", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void desconectar() {
+        try {
+            JOptionPane.showMessageDialog(null, "Desconectado", "desconectado", JOptionPane.INFORMATION_MESSAGE);
+            conn.close();
+            
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Error ao Desconectar", erro + "desconectado", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
